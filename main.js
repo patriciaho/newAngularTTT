f6313d00a699f3c2b613d83c920e604a8e8673d2
patriciaho <patricia.p.ho@gmail.com>
2014-01-16 02:01:27 -0800

    Link index.html to new files main.js and style.css

---
+++ main.js
@@ -0,0 +1,68 @@
+function allCTRL($scope) {
+	$scope.board = [];
+	$scope.xTurn = {val:true};
+	gameover = false;
+
+	$scope.makeBoard = function () {
+		for (var i = 0; i < ($scope.xsquared * $scope.xsquared); i++) {
+			$scope.board.push('');
+		}
+	}
+	
+	$scope.ticClick = function($index){
+		$scope.board[$index] = ($scope.board[$index] == '' ? (($scope.xTurn.val = !$scope.xTurn.val) ? 'X' : 'O' ) : $scope.board[$index]);
+		// alert($index+(2*$scope.xsquared));
+		if ($scope.board[$index] == 'X' && 
+			$scope.board[$index+1] == 'X' && 
+			$scope.board[$index+2] == 'X')
+			alert('Xwins!');
+		if ($scope.board[$index] == 'X' && 
+			$scope.board[$index-1] == 'X' && 
+			$scope.board[$index-2] == 'X')
+			alert('Xwins!');
+		if ($scope.board[$index] == 'X' && 
+			$scope.board[$index-1] == 'X' && 
+			$scope.board[$index+1] == 'X')
+			alert('Xwins!');
+		if ($scope.board[$index] == 'X' && 
+			$scope.board[$index + $scope.xsquared] == 'X' && 
+			$scope.board[$index + (2*$scope.xsquared)] == 'X')
+			alert('Xwins!');
+		if ($scope.board[$index] == 'X' && 
+			$scope.board[$index - $scope.xsquared] == 'X' && 
+			$scope.board[$index - (2*$scope.xsquared)] == 'X')
+			alert('Xwins!');
+		if ($scope.board[$index] == 'X' && 
+			$scope.board[$index - $scope.xsquared] == 'X' && 
+			$scope.board[$index + $scope.xsquared] == 'X')
+			alert('Xwins!');
+		if ($scope.board[$index] == 'X' && 
+			$scope.board[$index + $scope.xsquared + 1] == 'X' && 
+			$scope.board[$index + (2*($scope.xsquared)) +2] == 'X')
+			alert('Xwins!');
+		if ($scope.board[$index] == 'O' && 
+			$scope.board[$index+1] == 'O' && 
+			$scope.board[$index+2] == 'O')
+			alert('Owins!');
+		if ($scope.board[$index] == 'O' && 
+			$scope.board[$index-1] == 'O' && 
+			$scope.board[$index-2] == 'O')
+			alert('Owins!');
+		if ($scope.board[$index] == 'O' && 
+			$scope.board[$index-1] == 'O' && 
+			$scope.board[$index+1] == 'O')
+			alert('Owins!');
+		if ($scope.board[$index] == 'O' && 
+			$scope.board[$index + $scope.xsquared] == 'O' && 
+			$scope.board[$index + (2*$scope.xsquared)] == 'O')
+			alert('Owins!');
+		if ($scope.board[$index] == 'O' && 
+			$scope.board[$index - $scope.xsquared] == 'O' && 
+			$scope.board[$index - (2*$scope.xsquared)] == 'O')
+			alert('Owins!');
+		if ($scope.board[$index] == 'O' && 
+			$scope.board[$index - $scope.xsquared] == 'O' && 
+			$scope.board[$index + $scope.xsquared] == 'O')
+			alert('Owins!');
+	};
+}
