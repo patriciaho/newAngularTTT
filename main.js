f8508634c1c7f3c2be6a0c6af8b0e35d4f9d958e
patriciaho <patricia.p.ho@gmail.com>
2014-01-16 22:56:17 -0800

    Restore click and turn function

--- main.js
+++ main.js
@@ -4,20 +4,7 @@ function allCTRL($scope) {
 	gameover = false;
 
 	$scope.makeBoard = function () {
-		// for (var i = 0; i < ($scope.xsquared); i++) {
-		// 	$scope.board.push('');
-		// }
-
-		// for(i = 0; i < ($scope.xsquared); i++) {
-  //       	$scope.board.push([null]);
-  //       	$scope.board[i].push
-  //       	// for (j = 0; j < ($scope.xsquared); j++) {
-  //       	// 	$scope.board[j].push(null);
-  //       	// }
-  //   	}
-
     	var result = new Array();
-
 			for(var i = 0; i < ($scope.xsquared); i++) {
 			  var arr = new Array();
 			  for(var j = 0; j < ($scope.xsquared); j++) {
@@ -25,20 +12,7 @@ function allCTRL($scope) {
 			  }
 			  result.push(arr);
 		}
-
-		$scope.board = result
-
-    	// console.log($scope.board[i])
-
-    	// for(i = 0; i < ($scope.xsquared); i++) {
-     //    	$scope.board[i].push('hi');
-    	// }
-    	// console.log($scope.board)
-
-		// for (var i = 0; i < ($scope.xsquared); i++) {
-		// 		$scope.board[i].push('');
-		// }
-		
+		$scope.board = result	
 	}
 
 	$scope.mainSize = function () {
@@ -55,67 +29,12 @@ function allCTRL($scope) {
 		};
 	}
 	
-	$scope.ticClick = function($index){
-		if ($scope.board[$index] == '')
+	$scope.ticClick = function(r, c){
+		if ($scope.board[r][c] == '')
 			$scope.xTurn.val = !$scope.xTurn.val;
 			if ($scope.xTurn.val == true)
-				$scope.board[$index] = 'X';
+				$scope.board[r][c] = 'X';
 			else
-				$scope.board[$index] = 'O';
-
-		// $scope.board[$index] = ($scope.board[$index] == '' ? (($scope.xTurn.val = !$scope.xTurn.val) ? 'X' : 'O' ) : $scope.board[$index]);
-		// alert($index+(2*$scope.xsquared));
-		if ($scope.board[$index] == 'X' && 
-			$scope.board[$index+1] == 'X' && 
-			$scope.board[$index+2] == 'X')
-			alert('Xwins!');
-		if ($scope.board[$index] == 'X' && 
-			$scope.board[$index-1] == 'X' && 
-			$scope.board[$index-2] == 'X')
-			alert('Xwins!');
-		if ($scope.board[$index] == 'X' && 
-			$scope.board[$index-1] == 'X' && 
-			$scope.board[$index+1] == 'X')
-			alert('Xwins!');
-		if ($scope.board[$index] == 'X' && 
-			$scope.board[$index + $scope.xsquared] == 'X' && 
-			$scope.board[$index + (2*$scope.xsquared)] == 'X')
-			alert('Xwins!');
-		if ($scope.board[$index] == 'X' && 
-			$scope.board[$index - $scope.xsquared] == 'X' && 
-			$scope.board[$index - (2*$scope.xsquared)] == 'X')
-			alert('Xwins!');
-		if ($scope.board[$index] == 'X' && 
-			$scope.board[$index - $scope.xsquared] == 'X' && 
-			$scope.board[$index + $scope.xsquared] == 'X')
-			alert('Xwins!');
-		if ($scope.board[$index] == 'X' && 
-			$scope.board[$index + $scope.xsquared + 1] == 'X' && 
-			$scope.board[$index + (2*($scope.xsquared)) +2] == 'X')
-			alert('Xwins!');
-		if ($scope.board[$index] == 'O' && 
-			$scope.board[$index+1] == 'O' && 
-			$scope.board[$index+2] == 'O')
-			alert('Owins!');
-		if ($scope.board[$index] == 'O' && 
-			$scope.board[$index-1] == 'O' && 
-			$scope.board[$index-2] == 'O')
-			alert('Owins!');
-		if ($scope.board[$index] == 'O' && 
-			$scope.board[$index-1] == 'O' && 
-			$scope.board[$index+1] == 'O')
-			alert('Owins!');
-		if ($scope.board[$index] == 'O' && 
-			$scope.board[$index + $scope.xsquared] == 'O' && 
-			$scope.board[$index + (2*$scope.xsquared)] == 'O')
-			alert('Owins!');
-		if ($scope.board[$index] == 'O' && 
-			$scope.board[$index - $scope.xsquared] == 'O' && 
-			$scope.board[$index - (2*$scope.xsquared)] == 'O')
-			alert('Owins!');
-		if ($scope.board[$index] == 'O' && 
-			$scope.board[$index - $scope.xsquared] == 'O' && 
-			$scope.board[$index + $scope.xsquared] == 'O')
-			alert('Owins!');
+				$scope.board[r][c] = 'O';
 	};
 }
